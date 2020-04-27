8b91df9bf95e33eebeba380c41cacc011819ee93
caiden kehrer <abbotjunk@gmail.com>
2020-04-27 15:42:34 -0600

    First commit Cat tell if you are a mac or windows user

---
+++ TBG-UNITAL/Filemanager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+namespace TBG_UNITAL
+{
+    class FileManager
+    {
+        List<string> Keys = new List<string>();
+        readonly List<Stream> files = new List<Stream>();
+        string filebase;
+        public FileManager(string _base)
+        {
+            filebase = _base;
+        }
+        public bool AddFile(string _source, string _key)
+        {
+            Stream stream;
+            return File.Exists(_source);
+        }
+    }
+}
---
+++ TBG-UNITAL/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+namespace TBG_UNITAL
+{
+    class Program
+    {
+        static bool debug = true;
+        static FileManager man;
+        static void Main(string[] args)
+        {
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                if (debug) Console.WriteLine("You are a mac");
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                if (debug) Console.WriteLine("You are windows");
+            }
+            //man = new FileManager();
+            Console.Write(man.AddFile("test","baina").ToString());
+        }
+
+    }
+    
+}
